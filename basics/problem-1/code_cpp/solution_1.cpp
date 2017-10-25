e1a2ae6af6c07b75d89269938726ba7f2039005b
olonlog <olonlog-imac-05@adminMac.local>
2017-10-25 14:25:08 +0800

    comment added

--- basics/problem-1/code_cpp/solution_1.cpp
+++ basics/problem-1/code_cpp/solution_1.cpp
@@ -1,3 +1,9 @@
+/**
+    @file    solution_1.cpp
+    @author  Altantur Bayarsaikhan (altantur)
+    @purpose Sum of 2 numbers
+    @version 1.0 25/10/17 
+*/
 #include <iostream>
 #include <fstream>
 using namespace std;
@@ -6,11 +12,13 @@ int main(){
     ifstream test_file;
     int a = 0, b = 0, sum = 0;
 
+    // Read from test files
     test_file.open ("../test/test1.txt");
     test_file >> a;
     test_file >> b;
     test_file.close();
 
+    // Find sum of a and b
     sum = a + b;
 
     cout << sum << endl;
