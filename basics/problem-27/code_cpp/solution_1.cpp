3a00695c9941201bbb7bc811330c83012cffaa84
altantur <turuu.br@gmail.com>
2017-11-05 12:35:41 +0800

    reverse integer digits solved

--- basics/problem-27/code_cpp/solution_1.cpp
+++ basics/problem-27/code_cpp/solution_1.cpp
@@ -1,8 +1,8 @@
 /**
-    @file    sum_of_2_solution1.cpp
+    @file    reverse_digits_solution1.cpp
     @author  Altantur Bayarsaikhan (altantur)
-    @purpose Find sum of 2 integers
-    @version 1.0 25/10/17 
+    @purpose Reverse the digits of given integer
+    @version 1.0 25/10/17
 */
 #include <iostream>
 #include <fstream>
@@ -10,12 +10,21 @@ using namespace std;
 
 int main(){
     ifstream test_file;
+    int a, sum = 0;
 
     // Read from test files
-    test_file.open ("../test/test1.txt");
-    test_file.close();
+    // test_file.open ("../test/test1.txt");
+    // test_file.close();
 
+    // Get user input
+    cin >> a;
+    while(a > 0){
+      sum *= 10;
+      sum += a % 10;
+      a /= 10;
+    }
+
+    cout << "Reversed : " << sum << endl;
 
     return 0;
 }
-
