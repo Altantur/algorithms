66524839ee784739bbda8fe8d3ffdb8473fd9b70
altantur <turuu.br@gmail.com>
2017-11-06 11:36:06 +0800

    linear search array

---
+++ basics/problem-32/code_cpp/linear_search_solution1.cpp
@@ -0,0 +1,44 @@
+/**
+    @file    linear_search_solution1.cpp
+    @author  Altantur Bayarsaikhan (altantur)
+    @purpose Find given value from array, if nothing found print "No"
+    @version 1.0 25/10/17 
+*/
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(){
+    // ifstream test_file;
+    int n, k, i;
+
+    // Read from test files
+    // test_file.open ("../test/test1.txt");
+    // test_file.close();
+
+    // Getting user input
+    cin >> k;
+    cin >> n;
+    int a[n];
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+
+    // Break from first occurance
+    for(i = 0; i < n; i++){
+        if(a[i] == k) break; 
+    }
+
+    // Assure if it is real one or invalid value
+    if(n > 0){
+       if(a[i] == k){
+          cout << "Index is : " << i << endl; 
+       } 
+       else{
+          cout << "No" << endl;
+       }
+    }
+
+    return 0;
+}
+
