29707ad96efef41b029ba5c29562c24c7d485066
altantur <turuu.br@gmail.com>
2017-11-14 15:31:55 +0800

    math is fun

--- basics/problem-18/code_cpp/sum_inc_3_solution2.cpp
+++ basics/problem-18/code_cpp/sum_inc_3_solution2.cpp
@@ -10,22 +10,27 @@ using namespace std;
 
 int main(){
     ifstream test_file;
-    int m = 0, n = 0, sum = 0;
+    int m = 0, n = 0, sum = 0, count;
 
     // Read from test files
     test_file.open ("../test/test1.txt");
     test_file >> m;
     test_file >> n;
     test_file.close();
-
     while(m % 3 != 0){
         m ++;
     }
-
-    // Incrementing by 3 to limit and adding to sum.
-    for(int i = m; i <= n; i += 3){
-        sum += i;
+    while(m % 3 != 0){
+        n --;
     }
+
+    // Arithmetic sequence
+    count = n - m + 3;
+    count /= 3;
+
+    sum = n + m;
+    sum *= count;
+    sum /= 2;
 
     cout << "Sum of numbers that can divided by 3 : " << sum << endl;
 
