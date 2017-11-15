fde773151a8d34229bd26dcc65ccef3db0500f00
altantur <turuu.br@gmail.com>
2017-11-15 14:48:35 +0800

    space vs tab

--- basics/problem-34/code_cpp/change_sorted_array_solution1.cpp
+++ basics/problem-34/code_cpp/change_sorted_array_solution1.cpp
@@ -1,8 +1,8 @@
 /**
     @file    change_sorted_array_solution1.cpp
     @author  Altantur Bayarsaikhan (altantur)
-    @purpose Find out if only value changed then array can be sorted 
-    @version 1.0 25/10/17 
+    @purpose Find out if only value changed then array can be sorted
+    @version 1.0 25/10/17
 */
 #include <iostream>
 #include <fstream>
@@ -20,15 +20,15 @@ int main(){
     cin >> n;
     int a[n];
 
-    for(int i = 0; i < n; i ++)
+    for (int i = 0; i < n; i ++)
         cin >> a[i];
 
-    // To try to mandotary change
+    // To try to find at least one change
     for (int i = 1; i < n; i ++){
         if (i - 1 == 0 && a[i-1] > a[i]){
             a[i-1] = a[i];
             changes ++;
-        } 
+        }
         if (i + 1 == n - 1 && a[i+1] < a[i]){
             a[i+1] = a[i];
             changes ++;
@@ -40,6 +40,7 @@ int main(){
         }
     }
 
+    // After above only made mandotary change
     // Then check lefted change
     for (int i = 1; i < n; i ++){
         if(a[i-1] > a[i])
@@ -47,7 +48,6 @@ int main(){
     }
 
     changes > 1 ? cout << "No" << endl : cout << "Yes" << endl;
-        
+
     return 0;
 }
-
