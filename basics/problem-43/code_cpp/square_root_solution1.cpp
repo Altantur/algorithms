9b9a1c5b9a992fb9b39327b1d7884d06346d3f03
altantur <turuu.br@gmail.com>
2017-11-20 16:30:53 +0800

    square find manually problem context

---
+++ basics/problem-43/code_cpp/square_root_solution1.cpp
@@ -0,0 +1,19 @@
+/**
+    @file    square_root_solution1.cpp
+    @author  Altantur Bayarsaikhan (altantur)
+    @purpose Find square root of given non-negative integer X
+    @version 1.0 15/11/17
+*/
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(){
+    // Read from test files
+    // ifstream test_file;
+    // test_file.open ("../test/test1.txt");
+    // test_file.close();
+
+
+    return 0;
+}
