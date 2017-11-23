15299c9b5371b2e1236e2ee93df08e31ca49717d
altantur <turuu.br@gmail.com>
2017-11-23 15:37:50 +0800

    binary search single loop done

---
+++ methods/binary_search.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int search(vector<int> &a, int x) {
+  // Preventing to call size function every iteration
+  int m; // Index of middle element
+  size_t n = a.size(); // Length of vector
+  int l = 0, r = n - 1;
+  cout << endl << n << endl;
+
+  while (l <= r) {
+    m = l + (r - l) / 2;
+    if (a[m] == x) {
+      return m;
+    }
+    if (a[m] < x) {
+      l = m + 1;
+    } else {
+      r = m - 1;
+    }
+  }
+  return -1;
+}
+
+int main(){
+
+  // Getting user input and declaring vars
+  int x;
+  cin >> x; // This will be our find value
+  size_t n;
+  cin >> n; // Given sorted array size
+  vector<int> a(n);
+
+  for (size_t i = 0; i < n; i++)
+    cin >> a[i];
+
+  cout << "Index is : " << search(a, x) << endl;
+
+  return 0;
+}
